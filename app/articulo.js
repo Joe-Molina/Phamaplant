4d3075c6933899347e302b046300265874f510cb
Joe Molina <joedodaniljr123@gmail.com>
2023-06-28 14:41:15 -0400

    casi listo

--- app/articulo.js
+++ app/articulo.js
@@ -1,31 +1,86 @@
-const d = document,
-    $template = d.getElementById('crud-template').content,
-    $template2 = d.getElementById('componente-card-template').content,
-    $template3 = d.getElementById('beneficios-card-template').content,
-    $fragment = d.createDocumentFragment(),
-    $fragment2 = d.createDocumentFragment(),
-    $fragment3 = d.createDocumentFragment();
-    
+const d = document; 
+$template2 = d.getElementById('componente-card-template').content,
+$template3 = d.getElementById('beneficios-card-template').content,
+$fragment2 = d.createDocumentFragment(),
+$fragment3 = d.createDocumentFragment();
 
-d.addEventListener('DOMContentLoaded', e =>{
-    medicamentos();
-
-})
 
 d.addEventListener('click', e =>{
-    medicamento = 0
-    d.querySelectorAll('.cards-container article').forEach(el => {
-        medicamento++;
-        if(e.target.closest(`.medicamento${medicamento}`)){
-            d.querySelector('.medicamnetohidden').dataset.medica = `${medicamento}`
-            console.log(`tu mama ${medicamento}`);
+
+        if(e.target.closest(`.magnesium`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '28'
             d.querySelector('.seccion-container').style.opacity = 1;
             d.querySelector('.seccion-container').style.visibility = "visible"
             medicamentoinfo()
         }
+
+        if(e.target.closest(`.glucosamine`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '29'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.omega3`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '8'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.omega369`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '7'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.colagenovitaminac`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '25'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.acidohialuronicovitaminac`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '26'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.centella`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '3'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.slimplant`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '2'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.cental`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '1'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+        if(e.target.closest(`.vinagremanzana`)){
+            d.querySelector('.medicamnetohidden').dataset.medica = '27'
+            d.querySelector('.seccion-container').style.opacity = 1;
+            d.querySelector('.seccion-container').style.visibility = "visible"
+            medicamentoinfo()
+        }
+
+
         
         
-    });
+
     
     if(e.target.closest('.seccion-container .seccion-info .btn')){
         d.querySelector('.seccion-container').style.opacity = 0;
@@ -69,37 +124,14 @@ d.addEventListener('mouseout', e =>{
     
 })
 
-const medicamentos = async () =>{
-    try {
-        let res = await axios.get("../app/medicamentos.json");
-        let json = res.data.Medicamento;
-
-
-        json.forEach(el => {
-            $template.querySelector(".name").textContent = el.nombre;
-            $template.querySelector(".card-img").setAttribute('src', `../assets/potes/${el.foto}`)
-            $template.querySelector(".product-card").classList.remove(`medicamento${el.id - 1}`)
-            $template.querySelector(".product-card").classList.add(`medicamento${el.id}`)
-
-            let $clone = d.importNode($template, true)
-            $fragment.appendChild($clone);
-        });
-  
-        d.querySelector('.cards-container').appendChild($fragment)
-        } catch (err) {
-            let message = err.statusText || "Ocurrio un error";
-            console.error(message)
-            }
-}
-
 const medicamentoinfo = async () =>{
     try {
         let selectMedicamento = d.querySelector('.medicamnetohidden').dataset.medica
-
-        let res = await axios.get("../app/medicamentos.json");
+        
+        let res = await axios.get("../../app/medicamentos.json");
         let json = res.data.Medicamento[selectMedicamento - 1];
-
-        d.querySelector('.seccion-img img').src = `../assets/potes/${json.foto}`;
+        
+        d.querySelector('.seccion-img img').src = `../../assets/potes/${json.foto}`;
         d.querySelector('.seccion-info .seccion-name').textContent = json.nombre;
         d.querySelector('.seccion-info .seccion-funcion').textContent = json.Funcion;
         d.querySelector('.seccion-info .seccion-descripcion').textContent = json.descripcion;
@@ -110,7 +142,7 @@ const medicamentoinfo = async () =>{
         json.componentes.forEach(el => {
             nComponente++
             console.log(el.foto)
-            $template2.querySelector('.componente-div .componente-img').src = `../assets/componentes/${el.foto}`
+            $template2.querySelector('.componente-div .componente-img').src = `../../assets/componentes/${el.foto}`
             $template2.querySelector('.componente-div .componente-p').textContent = el.definicion;
             $template2.querySelector('.componente-div .componente-nombre').textContent = el.nombre;
             $template2.querySelector('.componente-div').classList.remove(`componente${nComponente-1}`)
@@ -139,8 +171,3 @@ const medicamentoinfo = async () =>{
             console.error(message)
             }
 }
-
-
-
-
-    
